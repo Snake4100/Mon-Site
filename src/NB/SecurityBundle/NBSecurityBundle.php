acfd9fcae4eeab076b902b7a73b84b9471f7fc8a
Snake4100 <msn.neilzb@hotmail.fr>
2014-12-14 21:07:39 +0100

    Login ancienne version

--- src/NB/SecurityBundle/NBSecurityBundle.php
+++ src/NB/SecurityBundle/NBSecurityBundle.php
@@ -2,18 +2,10 @@
 
 namespace NB\SecurityBundle;
 
-use NB\SecurityBundle\DependencyInjection\Security\Factory\WsseFactory;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 
 class NBSecurityBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
 
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new WsseFactory());
-    }
 }
